b59a8dd4418882cb37b27fe65ccd23826376774f
Danny Dulai <danny@ishiboo.com>
2013-06-29 18:28:19 -0400

    no more server pkg

--- server/buildpkg.sh
+++
@@ -1,7 +0,0 @@
-set -x
-rm -rf vooserver.app
-cp -a vooserver/bin/Release/vooserver.app .
-cp -a vooplayer/bin/Release/vooplayer.app vooserver.app/Contents/Resources/
-du -a vooserver.app
-echo exit 0 > vooserver.app/Contents/Resources/vooplayer.app/Contents/MacOS/mono-version-check
-chmod +x vooserver.app/Contents/Resources/vooplayer.app/Contents/MacOS/mono-version-check
